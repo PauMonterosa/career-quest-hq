7bf97db59355de89841f94f05cb98c65ce4f29c8
Pau Monterosa <paumonterosa@gmail.com>
2026-07-31 22:13:50 +0200

    feat: introduce minimal HQ emblem

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "career-quest-hq-v9";
+const CACHE = "career-quest-hq-v10";
 const base = new URL(self.registration.scope).pathname;
 const asset = path => `${base}${path}`;
 const APP_SHELL = [base, asset("manifest.webmanifest"), asset("icons/icon-192.png"), asset("icons/icon-512.png")];
